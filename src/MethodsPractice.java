b9fa1641e535ea8556d3a457139eee58bdffb9e3
Damon Castillo <damono7.castillo@gmail.com>
2022-06-13 12:20:42 -0500

    MethodsPractice completed

---
+++ src/MethodsPractice.java
@@ -0,0 +1,13 @@
+
+
+
+
+public class MethodsPractice {
+    public static int getDifference(int i,int j){
+        return (i - j );
+    };
+
+    public static void main(String[] args) {
+        System.out.println((getDifference(10, 5)));
+    }
+}
